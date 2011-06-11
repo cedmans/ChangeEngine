0ae86acabe83c822a6fb2facdce3b8260c99324a
Cedric Wienold <cwienold@gmail.com>
2011-06-10 19:22:50 -0700

    Forgot to add event listener class

---
+++ implementation/ChangeEngine/EventListener.cpp
@@ -0,0 +1,27 @@
+/**
+ *  @file EventListener.cpp
+ *
+ *  @date May 21, 2011
+ *  @author Cedric Wienold
+ */
+
+#include "debug.hpp"
+
+#include "EventListener.hpp"
+#include "EventTypes.hpp"
+#include "errorcodes.hpp"
+
+bool EventListener::pollEvent() {
+
+   return SDL_PollEvent(&event);
+}
+
+int EventListener::getEvent() {
+
+   return event.type;
+}
+
+int EventListener::getKey() {
+
+   return event.key.keysym.sym;
+}
