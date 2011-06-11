2e5c214ef1a61f818fe21f3f37ec9a3688614ca3
Cedric Wienold <cwienold@gmail.com>
2011-06-10 19:26:30 -0700

    Forgot to put in a working makefile for tester

--- implementation/ChangeEngineTester/main.cpp
+++ implementation/ChangeEngineTester/main.cpp
@@ -28,12 +28,36 @@ int main(int argc, char **argv) {
    engine->setWindowCaption("Event Test");
 
    engine->createWindow(SCREENWIDTH,SCREENHEIGHT,SCREENBPP);
+   
+   /** Make a level */
+   engine->createLevel("Level1");
+   
+   /** Construct the ship */
+//   engine->createGameObject("Level1","Ship");
+   //Give the ship an image with tile width and height of 36x33
+/*   if (engine->attachImageToGameObject("Level1","Ship","./images/spaceship.png",36,33) != EENGINE_SUCCESS) {
+      
+      fprintf(stderr,"Could not attach image!\n");
+      fflush(stderr);
+      return -1;
+   }
+   //Add states
+   engine->addAvatarState("Level1","Ship",3);
+   engine->addAvatarState("Level1","Ship",3);
+   engine->addAvatarState("Level1","Ship",3);
+   fflush(stderr);*/
 
    /** RUN GAME */
 
    int event, key;
 
    while (gameRunning) {
+
+//      if (engine->drawObject("Level1","Ship",0,0) != EENGINE_SUCCESS)
+//         fprintf(stderr,"*** DRAW FAILED ***\n");
+      
+//      if (SDL_Flip(engine->getWindow()->getScreen()) != 0) 
+//         fprintf(stderr,"*** FLIP FAILED ***\n");
 
       while( engine->getEventListener()->pollEvent()) {
 
@@ -48,6 +72,26 @@ int main(int argc, char **argv) {
                fprintf(stdout,"Tester: Received CE_KEYDOWN\n");
 
                key = engine->getEventListener()->getKey();
+               
+               switch(key) {
+                  
+                  case CE_KB_ESCAPE:
+                     fprintf(stdout,"Tester: Received CE_KB_ESCAPE\n");
+                     gameRunning = false;
+                     break;
+                  default:
+                     fprintf(stdout,"Tester: Received CE_KB_somethingelse\n");
+                     break;
+               }
+               
+               break;
+            case CE_KEYUP:
+               fprintf(stdout,"Tester: Received CE_KEYUP\n");
+               
+               key = engine->getEventListener()->getKey();
+               
+               break;
+            default:
                break;
          }
       }
